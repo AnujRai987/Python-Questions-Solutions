cc7686d59df786bbde62cdae7e643d8d83c6c241
AnujRai987 <onlyforstudypurpose2023@gmail.com>
2026-01-13 20:45:46 +0530

    youtube manager with db

---
+++ python_prac/youtube_manager_db.py
@@ -0,0 +1,32 @@
+def list_all_videos():
+    pass
+def add_video():
+    pass 
+
+def update_video():
+    pass
+def delete_video():
+    pass
+
+
+def main():
+    while True:
+        print("\nYoutube Manager | Make a choice : \n1.List all videos\n2.Add Video in favorite\n3.Update video details\n4.Delete Video\n5.Exit\n")
+        choice=input("Enter your choice : ")
+
+        match choice:
+            case '1':
+                list_all_videos()
+            case '2':
+                add_video()
+            case '3':
+                update_video()
+            case '4':
+                delete_video()
+            case '5':
+                break
+            case _:
+                print("Invalid choice !!")
+
+if __name__ == "__name__":
+    main()
