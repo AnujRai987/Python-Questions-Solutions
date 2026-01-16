2f8d53a87c56800507698f8f91ce96fd11e90d4f
AnujRai987 <onlyforstudypurpose2023@gmail.com>
2026-01-16 22:17:40 +0530

    api response

--- python_prac/api_requests.py
+++ python_prac/api_requests.py
@@ -1,7 +1,23 @@
 import requests
 
+
 url="https://api.freeapi.app/api/v1/public/randomproducts/product/random"
 response = requests.get(url)
 data =response.json()
 
-print(data["data"]["id"])
+info=data['data'] # storing data inside response data
+
+if data['success']==True and 'data' in data:
+    print(data)
+    print(data['statusCode'])
+    print(data['data']['title'])
+    print(data['data']['description'])
+
+
+    print('\n\n\n')
+    for key,value in info.items():
+        print(f"{key}={value}")
+
+
+
+
